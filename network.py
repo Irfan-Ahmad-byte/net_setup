d3cdaee01b23e51fcaae6dfa836086abc2a6b250
Irfan-Ahmad-byte <irfan.ahmad.mlka@gmail.com>
2022-05-30 11:45:46 +0500

    net

--- network.py
+++ network.py
@@ -1,6 +1,13 @@
+import argparse
 import json
 from mininet.net import Mininet
 from mininet.topo import Topo
+from mininet.cli import CLI
+from mininet.log import lg
+from mininet.node import RemoteController
+from mininet.net import Mininet
+from mininet.topo import Topo
+from mininet.link import TCLink
 
 # make bytes into long
 # source http://stackoverflow.com/questions/25259947/convert-variable-sized-byte-array-to-a-integer-long
@@ -15,12 +22,9 @@ def clean_hex(n):
 
 class customTopo(Topo):
     """ create a custom topology"""
-    def __init__(self, **opts):
+    def __init__(self,graph, **opts):
         listenPort = 6653
         Topo.__init__(self, **opts)
-        fl = open('network.json')
-        graph = json.load(fl)
-        fl.close()
         nodes = graph['nodes']
         node_names = {}
         for node in nodes: # node name as unicode str
@@ -45,3 +49,32 @@ class customTopo(Topo):
                          delay=delay)
 
 topos = {'customTopo': ( lambda: customTopo() )}
+
+if __name__ == '__main__':
+    fl = "network.json"  # You can put your default file here
+    remoteIP = "192.168.100.6"      # Put your default remote IP here
+    # Using the nice Python argparse library to take in optional arguments
+    # for file name and remote controller IP address
+    fl = open('network.json')
+    graph = json.load(fl)
+    fl.close()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--fl", help="network graph file name")
+    parser.add_argument("-ip", "--remote_ip", help="IP address of remote controller")
+    args = parser.parse_args()
+    if not args.fl:
+        print ("fl not specified using: {}".format(fl))
+    else:
+        fl = args.fl
+    if not args.remote_ip:
+        print ("remote controller IP not specified using: {}".format(remoteIP))
+    else:
+        remoteIP = args.remote_ip
+    topo = customTopo(graph)
+    lg.setLogLevel('info')
+    network = Mininet(controller=RemoteController, autoStaticArp=True, link=TCLink)
+    network.addController(controller=RemoteController, ip=remoteIP)
+    network.buildFromTopo(topo=topo)
+    network.start()
+    CLI(network)
+    network.stop()
